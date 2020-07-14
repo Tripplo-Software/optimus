143020755b7c52ed751542ef0fdcebcbceb33426
AmoDinho <u14284783@tuks.co.za>
2020-07-14 17:26:52 +0200

    editied tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,12 @@
 const postcss = require('rollup-plugin-postcss')
 const autoprefixer = require('autoprefixer')
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        plugins: [autoprefixer()],
+        plugins: [autoprefixer(), tailwindcss('./tailwind.config.js')],
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
