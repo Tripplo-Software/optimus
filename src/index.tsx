26ae56a2c0d1b843125b5f12e2414dc5be5bb412
AmoDinho <u14284783@tuks.co.za>
2020-07-14 17:42:09 +0200

    tailwind is working

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react'
-
+import './index.css'
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: ReactChild
 }
@@ -7,5 +7,7 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
 export const Thing: FC<Props> = ({}) => {
-  return <div>{`the snozzberries taste like snozzberries`}</div>
+  return (
+    <div className="bg-blue-500">{`the snozzberries taste like snozzberries`}</div>
+  )
 }
