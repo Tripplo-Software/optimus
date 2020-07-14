3727ddc59c5cc2112f9cb2e2bd8d2e4194064ae1
AmoDinho <u14284783@tuks.co.za>
2020-07-14 17:34:10 +0200

    adjusted the tsdx for the rollup

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,17 @@
 const postcss = require('rollup-plugin-postcss')
 const autoprefixer = require('autoprefixer')
 const tailwindcss = require('tailwindcss')
+const css = require('rollup-plugin-css-only')
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        plugins: [autoprefixer(), tailwindcss('./tailwind.config.js')],
+        plugins: [
+          autoprefixer(),
+          tailwindcss('./tailwind.config.js'),
+          css({ output: 'dist/optimus.css' }),
+        ],
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
