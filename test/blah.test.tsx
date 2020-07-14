b2b0bf7fc93f28f688416634762d4c0691e62771
AmoDinho <u14284783@tuks.co.za>
2020-07-14 17:13:24 +0200

    added the mathed

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import * as ReactDOM from 'react-dom'
 import { Thing } from '../src/index'
-import {
-  render,
-  cleanup,
-  waitForDomChange,
-  Matcher,
-  act,
-} from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 describe('Thing', () => {
   it('The text matches the prop passed to it', () => {
     afterEach(cleanup)
 
     //create test children
+    // const childern = 'Hush'
+    const { getByText } = render(<Thing />)
 
+    const div = 'the snozzberries taste like snozzberries'
+    expect(getByText(div)).toHaveTextContent(
+      'the snozzberries taste like snozzberries'
+    )
     //execute the getByText'
   })
 })
