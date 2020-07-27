6e548e4e4b7016fa8eb9344fd9e136b6d0f6f9a8
AmoDinho <u14284783@tuks.co.za>
2020-07-27 08:36:17 +0200

    added default props

--- src/inputs/Input.tsx
+++ src/inputs/Input.tsx
@@ -4,8 +4,13 @@ import '../index.css'
 export interface Props {
   className: string
 }
+
+const defaultProps = {
+  className: 'bg-white border border-gray-400',
+}
 const Input = (props: Props) => {
   return <input {...props} />
 }
 
+Input.defaultProps = defaultProps
 export default Input
