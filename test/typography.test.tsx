a9d0a248debdb9089be91caa187197e74760fcda
AmoDinho <u14284783@tuks.co.za>
2021-03-12 10:00:42 +0200

    added the render

--- test/typography.test.tsx
+++ test/typography.test.tsx
@@ -1,4 +1,14 @@
 import React from 'react'
-import { Thing } from '../src/index'
+import { HeadingOne } from '../src/index'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+
+describe('Typography', () => {
+  it('[HeadingOne] - It matches the Text prop passed to it', () => {
+    afterEach(cleanup)
+
+    const { getByText } = render(
+      <HeadingOne className="text-green-3000">Hello there</HeadingOne>
+    )
+  })
+})
