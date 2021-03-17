84f3d337e3dbd72a2cc16f4aff28ed8c605564fd
Vesta-Nassone <vestanassone@gmail.com>
2021-03-17 11:02:42 +0200

    Implements  BlueActionButton component.

---
+++ src/buttons/BlueActionButton.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+import '../index.css'
+
+export interface Props {
+  className: string
+  onClick: any
+  children: string
+  disabled: boolean
+}
+
+export const BlueActionButton: FC<Props> = ({
+  className,
+  onClick,
+  children,
+  disabled,
+}: Props) => (
+  <button
+    className={` ${className} bg-blue-500 text-center py-2 px-4 rounded-lg`}
+    onClick={onClick}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+);
