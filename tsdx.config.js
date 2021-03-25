1c7b215759be5d21913460b2aa90d2f360aa748a
Vesta-Nassone <vestanassone@gmail.com>
2021-03-25 07:32:46 +0200

    Fixes Font Issue. Poppin fonts now working.

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,7 +3,6 @@ const autoprefixer = require('autoprefixer')
 const tailwindcss = require('tailwindcss')
 const css = require('rollup-plugin-css-only')
 const cssnano = require('cssnano')
-// const svg = require('rollup-plugin-svg')
 
 module.exports = {
   rollup(config, options) {
@@ -15,8 +14,7 @@ module.exports = {
           tailwindcss('./tailwind.config.js'),
           cssnano({
             preset: 'default',
-          }),
-          // svg(),
+          })
         ],
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
