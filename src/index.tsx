4f4ac07f0df742e094fad8303706f2bc2f18c3fb
AmoDinho <u14284783@tuks.co.za>
2021-03-25 16:27:53 +0200

    Button is working now

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { default as Input } from './inputs/Input'
 export { default as Typography } from './typography/Typography'
 
-export { default as Button } from './buttons'
+export { default as Button } from './buttons/Button'
