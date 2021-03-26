1351f1e7e5811afe51f6f364eef7554f203234c7
Vesta-Nassone <vestanassone@gmail.com>
2021-03-26 14:45:52 +0200

    Adds a value to InputComponent.

--- src/inputs/Input.tsx
+++ src/inputs/Input.tsx
@@ -3,14 +3,15 @@ import '../index.css'
 
 export interface Props {
   className: string
+  placeholder: string
+  value?: string
 }
 
 const defaultProps = {
-  className: 'bg-white border border-gray-400 hover:border-blue-400 rounded p-2 w-auto',
+  className: 'bg-white border border-gray-400 hover:border-blue-400 focus:outline-none rounded py-2 w-auto',
 }
 const Input = (props: Props) => {
   return <input 
-  placeholder="Enter your message here"
   {...props} />
 }
 
