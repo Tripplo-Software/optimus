cbeb1087c7feed61405775876c4417713034e06b
Vesta-Nassone <vestanassone@gmail.com>
2021-03-26 13:56:07 +0200

    Removes commented out code.

--- stories/typography.stories.tsx
+++ stories/typography.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Typography } from '../src/typography'
-// import { storiesOf } from '@storybook/react'
 
 // By passing optional props to this story, you can control the props of the component when
 // you consume the story in a test.
@@ -41,41 +40,8 @@ export const BodyText = () => (
 export const LinkText = () => (
   <Typography
     variant="Link"
-    // className="text-blue-500 no-underline hover:underline"
     href={url}
   >
     This is a link
   </Typography>
 )
-
-// storiesOf('Typography', module)
-//   .add('Heading 1', () => (
-//     <Typography variant="HeadingOne">Heading 1</Typography>
-//   ))
-//   .add('Heading 2', () => (
-//     <Typography variant="HeadingTwo">Heading 2</Typography>
-//   ))
-//   .add('Heading 3', () => (
-//     <Typography variant="HeadingThree">Heading 3</Typography>
-//   ))
-//   .add('Heading 4', () => (
-//     <Typography variant="HeadingFour">Heading 4</Typography>
-//   ))
-//   .add('Heading 5', () => (
-//     <Typography variant="HeadingFive">Heading 5</Typography>
-//   ))
-//   .add('Heading 6', () => (
-//     <Typography variant="HeadingSix">Heading 6</Typography>
-//   ))
-//   .add('Body Text', () => (
-//     <Typography variant="BodyText">This is a paragraph</Typography>
-//   ))
-//   .add('Text Link', () => (
-//     <Typography
-//       variant="Link"
-//       // className="text-blue-500 no-underline hover:underline"
-//       href={url}
-//     >
-//       This is a link
-//     </Typography>
-//   ))
