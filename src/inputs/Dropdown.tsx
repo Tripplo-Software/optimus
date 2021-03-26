7df8bc956f40d874c47a834f1f61144fd266230c
Vesta-Nassone <vestanassone@gmail.com>
2021-03-26 14:13:09 +0200

    Adds aria-label to Dropdown Component.

--- src/inputs/Dropdown.tsx
+++ src/inputs/Dropdown.tsx
@@ -8,6 +8,7 @@ export interface Props {
 
 const Dropdown: FC<Props> = ({ children, className }: Props) => (
     <select
+        aria-label='dropdown-menu'
         className={`${className} border bg-white border-gray-300 w-auto p-2 hover:border-blue-400 rounded font-poppins `}
     >
         {children}
