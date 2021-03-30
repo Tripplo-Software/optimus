0a2917ad95561109465cd45a4a44e28d93ff1754
Vesta-Nassone <vestanassone@gmail.com>
2021-03-30 14:41:13 +0200

    Styles the Dropdown component.

--- src/inputs/dropdown/Dropdown.tsx
+++ src/inputs/dropdown/Dropdown.tsx
@@ -5,30 +5,31 @@ import 'antd/dist/antd.css'
 export const { Option } = Select
 
 export interface Props {
-  className?: string
-  children?: any
-  onChange:  any
-  onBlur: any
-  onFocus: any
-  onSearch: any
+    className?: string
+    children?: any
+    onChange: any
+    onBlur: any
+    onFocus: any
+    onSearch: any
+    placeholder?: string
 }
 
-const Dropdown: FC<Props> = ({ children, onBlur, onChange, onFocus, onSearch }: Props) => (
-  <Select
-     showSearch
-    style={{ width: 200 }}
-    placeholder="Select the currency"
-    optionFilterProp={children}
-    onChange={onChange}
-    onFocus={onFocus}
-    onBlur={onBlur}
-    onSearch={onSearch}
-    filterOption={(input: string, option: any) =>
-      option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-    }
-  >
-    {children}
-  </Select>
+const Dropdown: FC<Props> = ({ className, children, onBlur, onChange, onFocus, onSearch }: Props) => (
+    <Select
+        className={`${className} border border-gray-300 text-gray-600 h-10 pl-15 pr-10`}
+        showSearch
+        style={{ width: 200 }}
+        optionFilterProp={children}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onSearch={onSearch}
+        filterOption={(input: string, option: any) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+    >
+        {children}
+    </Select>
 )
 
 export default Dropdown
