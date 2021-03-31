7118bdb1a6400d7f295d61750ed44b93327c76e2
“Vesta <vestanassone@gmail.com>
2021-03-31 15:49:17 +0200

    Exports the DatePicker as module.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { default as Input } from './inputs/input/Input'
 export {default as Dropdown} from './inputs/dropdown/Dropdown'
 export {default as Option} from './inputs/dropdown/Dropdown'
+export {default as DatePicker} from './inputs/datePicker'
 export { default as Typography } from './typography/Typography'
 
 export { default as Button } from './buttons/Button'
---
+++ src/inputs/datePicker/index.tsx
@@ -0,0 +1,5 @@
+import DatePicker from './DatePicker'
+
+export { DatePicker }
+
+export default { DatePicker }
