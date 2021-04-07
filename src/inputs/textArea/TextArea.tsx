e445d2979b589559628bddce230435eaa80bbb2a
“Vesta <vestanassone@gmail.com>
2021-04-07 10:10:49 +0200

    Implements the TextArea component.

---
+++ src/inputs/textArea/TextArea.tsx
@@ -0,0 +1,25 @@
+import React, { FC, InputHTMLAttributes } from 'react'
+import '../../index.css'
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string
+  placeholder?: string
+  value: string
+  onChange: any
+}
+
+const TextArea: FC<Props> = ({
+  className,
+  onChange,
+  value
+}: Props) => {
+  return (
+    <textarea
+      className={` ${className} border border-grey font-poppins  py-12 px-16 rounded-sm focus:outline-none `}
+      onChange={onChange}
+      value={value}
+      data-testid="text-area"
+    />
+  )
+}
+
+export default TextArea;
