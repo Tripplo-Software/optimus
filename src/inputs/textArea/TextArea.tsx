eae9544c98b314ad38d40d50c77606c7bf362075
“Vesta <vestanassone@gmail.com>
2021-04-07 10:29:20 +0200

    Adds placeholer prop to TextArea.

--- src/inputs/textArea/TextArea.tsx
+++ src/inputs/textArea/TextArea.tsx
@@ -3,21 +3,24 @@ import '../../index.css'
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   className?: string
   placeholder?: string
-  value: string
+  value?: string
   onChange: any
 }
 
 const TextArea: FC<Props> = ({
   className,
   onChange,
-  value
+  value,
+  placeholder
 }: Props) => {
   return (
     <textarea
       className={` ${className} border border-grey font-poppins  py-12 px-16 rounded-sm focus:outline-none `}
       onChange={onChange}
       value={value}
+      placeholder={placeholder}
       data-testid="text-area"
+      
     />
   )
 }
