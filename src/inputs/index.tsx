bf0261d29bf3a06aeea050a95803aed30bc1a17b
“Vesta <vestanassone@gmail.com>
2021-04-07 10:08:42 +0200

    Exports the TextArea Component.

--- src/inputs/index.tsx
+++ src/inputs/index.tsx
@@ -1,8 +1,9 @@
 import Input from './input/Input'
 import Dropdown, { Option } from './dropdown/Dropdown'
 import DatePickerAPI from './datePicker/DatePickerAPI'
+import TextArea from './textArea/TextArea'
 
-export { Input, Dropdown, Option, DatePickerAPI }
+export { Input, Dropdown, Option, DatePickerAPI, TextArea }
 
-export default { Input, Dropdown, Option, DatePickerAPI }
+export default { Input, Dropdown, Option, DatePickerAPI, TextArea }
 
