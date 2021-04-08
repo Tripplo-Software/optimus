fd82472a479f5cb13c3659b2bb240f3b0d73b2e9
“Vesta <vestanassone@gmail.com>
2021-04-08 12:07:20 +0200

    Adds state to checkbox Component.

--- src/inputs/checkbox/Checkbox.tsx
+++ src/inputs/checkbox/Checkbox.tsx
@@ -3,27 +3,31 @@ import '../../index.css'
 
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
     className?: string
-    value?: string
-    name?: string
-    id?: string
+    onChange: any,
     children?: any
     variant: string
+    disabled?: boolean,
+    isChecked?: boolean
 }
 
 const Checkbox: FC<Props> = ({
     className,
+    onChange,
     variant,
     children,
-    ...rest
+    disabled=false,
+    isChecked=false
 }: Props) => {
     return (
         <label className="inline-flex items-center">
             <input
-                className={` ${className} form-checkbox`}
+                className={` ${className} form-checkbox h-4 w-4`}
+                onChange={onChange}
                 type={variant}
-                {...rest}
+                disabled={disabled}
+                checked={isChecked}
             />
-            <span className="ml-2">Placeholder</span>
+            <span className="ml-2">{children}</span>
         </label>
     )
 }
