9c305b46acd88d74457c256c98ac263f65219321
“Vesta <vestanassone@gmail.com>
2021-04-08 12:38:45 +0200

    Renames the DatePickerAPI component.

---
+++ src/inputs/datePicker/DatePicker.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import '../../index.css'
+import 'antd/dist/antd.css'
+import { DatePicker as Dpicker, Space } from 'antd'
+const { RangePicker } = Dpicker
+
+export interface Props {
+    className?: string
+    onChange: any
+    children: any
+    variant?: string
+    format?: string
+    value?: any
+}
+
+
+const DatePicker: FC<Props> = ({ className, onChange, variant = "datePicker", format = "DD/MM/YYYY", value }: Props) => (
+    <Space
+        className={`${className}`}
+        direction="vertical"
+    >
+        {variant === "rangePicker" ? (
+            <RangePicker onChange={onChange} />
+        ) : (
+            <Dpicker onChange={onChange}
+                format={format}
+                value={value}
+                data-testid="ant-picker"
+            />
+        )}
+    </Space>
+)
+
+export default DatePicker;
