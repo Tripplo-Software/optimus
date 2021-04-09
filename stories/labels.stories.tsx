f2ca2a59598b66d21103f84bd75b4d1db1d2c066
“Vesta <vestanassone@gmail.com>
2021-04-09 11:27:27 +0200

    Creates a Tag Story in Storybook.

---
+++ stories/labels.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Tag } from '../src/labels'
+
+export default {
+    title: 'Labels/Label',
+    component: Tag
+}
+
+export const WarningTag = () => (
+    <Tag></Tag>
+  )
