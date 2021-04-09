40fa6828fe465db85bdf52e6cbb0eeb82000b9b2
“Vesta <vestanassone@gmail.com>
2021-04-09 14:11:07 +0200

    Adds UnknownTag variant.

--- src/labels/Tag.tsx
+++ src/labels/Tag.tsx
@@ -16,24 +16,28 @@ const Tag: FC<Props> = ({
     const types: any = {
         SuccessTag: {
             backgroundColor: 'bg-green-300',
-            textUtils: 'text-white font-poppins rounded-lg',
+            textUtils: 'text-white font-poppins rounded-md',
         },
         WarningTag: {
             backgroundColor: 'bg-yellow-500',
-            textUtils: 'text-white font-poppins rounded-lg',
+            textUtils: 'text-white font-poppins rounded-md',
         },
         DangerTag: {
             backgroundColor: 'bg-red-300',
-            textUtils: 'text-white font-poppins rounded-lg',
+            textUtils: 'text-white font-poppins rounded-md',
         },
         ActiveTag: {
             backgroundColor: 'bg-green-300',
-            textUtils: 'text-white font-poppins rounded-lg',
+            textUtils: 'text-white font-poppins rounded-md',
         },
         InactiveTag: {
             borderColor: 'border border-solid border-red-200',
             backgroundColor: 'bg-transparent',
-            textUtils: 'text-gray-300 font-poppins rounded-lg',
+            textUtils: 'text-gray-300 font-poppins rounded-md',
+        },
+        UnknownTag: {
+            backgroundColor: 'bg-orange-400',
+            textUtils: 'text-gray-300 font-poppins rounded-full',
         }
     }
 
@@ -41,7 +45,7 @@ const Tag: FC<Props> = ({
 
     return (
         <div className={`${className} ${classNames} text-sm inline-flex items-center leading-sm px-3 py-1`}>
-                    {children}
+            {children}
         </div>
     )
 }
